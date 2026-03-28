fdd0b00158084d0064e1f0419ae5ceddbf508eb1
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-03-28 11:39:52 -0500

    feat: 🎸 enhance homepage UI and user messaging

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,44 +2,43 @@ import { Link } from 'react-router';
 
 export default function Home() {
 	return (
-		<main className="flex flex-1 flex-col space-y-10 px-6 pt-2">
-			<h1 className="ml-2">
-				Welcome to <strong>MileMetrics</strong>, the perfect place to track your
-				mile!
-				{/* <Link
-					to="/add-miles"
-					className="font-medium tracking-wide text-indigo-600 underline"
-				>
-					here
-				</Link>
-				! */}
+		<main className="flex flex-1 flex-col space-y-12 px-6">
+			<h1 className="ml-2 flex flex-col text-lg">
+				<span>
+					Welcome to <strong>MileMetrics</strong>...
+				</span>
+				<span className="ml-3">
+					...tracking your miles made <em>easy</em>.
+				</span>
 			</h1>
 
-			<div className="space-y-2">
-				<h1 className="mb-3 ml-2">
-					Here are some tips to get the most use of your experience!
+			<div className="flex flex-1 flex-col">
+				<h1 className="ml-2 font-semibold tracking-tight">
+					Select an option to get moving today!
 				</h1>
 
-				<ul className="space-y-5">
+				<ul className="flex flex-1 flex-col justify-around">
 					<li className="flex flex-col rounded-2xl border border-slate-500 bg-slate-50 p-3 shadow-md">
 						<p className="text-md font-bold">Log your miles regularly</p>
 						<p className="pt-0.5 text-sm">
-							Adding entries daily helps keep your records accurate and easy to
-							manage.
+							Stay on top of your driving by logging trips as you go—it keeps
+							everything simple and up to date.
 						</p>
 					</li>
+
 					<li className="flex flex-col rounded-2xl border border-slate-500 bg-slate-50 p-3 shadow-md">
 						<p className="text-md font-bold">Review your dashboard</p>
 						<p className="pt-0.5 text-sm">
-							Adding entries daily helps keep your records accurate and easy to
-							manage.
+							Get a clear view of your totals and trends so you always know how
+							your miles are adding up.
 						</p>
 					</li>
+
 					<li className="flex flex-col rounded-2xl border border-slate-500 bg-slate-50 p-3 shadow-md">
 						<p className="text-md font-bold">Stay organized (coming soon)</p>
 						<p className="pt-0.5 text-sm">
-							Adding entries daily helps keep your records accurate and easy to
-							manage.
+							Easily sort and manage your entries to keep everything neat and
+							easy to find.
 						</p>
 					</li>
 				</ul>
