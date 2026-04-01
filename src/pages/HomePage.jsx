da46f67843fd8ea50bdef0eaab9f3c5e896094d4
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-04-01 11:54:00 -0500

    feat: 🎸 implement xs breakpoint for HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ export default function Home() {
 		<main className="flex flex-1 flex-col px-6">
 			{/* Welcome Section */}
 			<div className="mb-10 flex justify-center py-6">
-				<h1 className="flex flex-col text-xl">
+				<h1 className="xs:text-2xl flex flex-col text-xl">
 					<span>
 						Welcome to{' '}
 						<span className="font-semibold text-emerald-700">MileMetrics</span>
@@ -17,24 +17,28 @@ export default function Home() {
 
 			{/* Cards Section */}
 			<div className="mb-12 flex flex-1 flex-col">
-				<h1 className="mb-3 flex justify-center text-sm font-semibold tracking-tight">
+				<h1 className="xs:text-base mb-3 flex justify-center text-sm font-semibold tracking-tight">
 					Select an option to get moving today!
 				</h1>
 
 				<ul className="flex flex-1 flex-col justify-between gap-6">
 					<Link to="add-miles">
-						<li className="flex flex-col rounded-2xl border border-slate-500 bg-slate-50 p-3 shadow-md hover:bg-emerald-100">
-							<p className="text-md font-bold">Log your miles regularly</p>
+						<li className="flex flex-col items-center rounded-2xl border border-slate-500 bg-slate-50 p-3 text-center shadow-md hover:bg-emerald-100">
+							<p className="xs:text-lg text-md mb-2 font-bold">
+								Log your miles regularly
+							</p>
 							<p className="pt-0.5 text-sm">
-								Stay on top of your driving by logging trips as you go—it keeps
-								everything simple and up to date.
+								Stay on top of your driving by logging trips as you go — it
+								keeps everything simple and up to date.
 							</p>
 						</li>
 					</Link>
 
 					<Link to="view-miles">
-						<li className="flex flex-col rounded-2xl border border-slate-500 bg-slate-50 p-3 shadow-md hover:bg-emerald-100">
-							<p className="text-md font-bold">Review your dashboard</p>
+						<li className="flex flex-col items-center rounded-2xl border border-slate-500 bg-slate-50 p-3 text-center shadow-md hover:bg-emerald-100">
+							<p className="xs:text-lg text-md mb-2 font-bold">
+								Review your dashboard
+							</p>
 							<p className="pt-0.5 text-sm">
 								Get a clear view of your totals and trends so you always know
 								how your miles are adding up.
@@ -43,8 +47,10 @@ export default function Home() {
 					</Link>
 
 					<Link>
-						<li className="flex flex-col rounded-2xl border border-slate-500 bg-slate-50 p-3 shadow-md hover:bg-emerald-100">
-							<p className="text-md font-bold">Stay organized (coming soon)</p>
+						<li className="flex flex-col items-center rounded-2xl border border-slate-500 bg-slate-50 p-3 text-center shadow-md hover:bg-emerald-100">
+							<p className="xs:text-lg text-md mb-2 font-bold">
+								Stay organized (coming soon)
+							</p>
 							<p className="pt-0.5 text-sm">
 								Easily sort and manage your entries to keep everything neat and
 								easy to find.
