1800e9d4a227da9af0c52606e8092ce9db809448
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-04-01 10:43:30 -0500

    refactor: 💡 rename Home to HomePage and move into pages dir

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router';
 import { RouterProvider } from 'react-router/dom';
-import Home from './ui/Home';
+import HomePage from './pages/HomePage';
 import AppLayout from './ui/AppLayout';
 import AddMilesPage from './pages/AddMilesPage';
 import { MileageProvider } from './features/mileage/MileageProvider';
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
 		children: [
 			{
 				path: '/',
-				element: <Home />,
+				element: <HomePage />,
 			},
 			{
 				path: '/add-miles',
