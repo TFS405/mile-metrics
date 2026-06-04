9397fa49fb977bab920badccebd4e139a126ea0f
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-06-03 20:40:58 -0500

    feat: 🎸 add flexible mileage entry filtering

--- src/services/apiMileage.js
+++ src/services/apiMileage.js
@@ -1,8 +1,46 @@
 import toast from 'react-hot-toast';
 import { supabase } from './supabase';
+import { getTodayDateString as todayDateString } from '../utils/dateUtils';
 
-export const getMileageEntries = async () => {
-	const { data, error } = await supabase.from('Miles').select('*');
+export const getMileageEntries = async ({
+	targetedDate = null,
+	startDate = null,
+	endDate = null,
+} = {}) => {
+	const hasTargetDate = Boolean(targetedDate);
+	const hasStartDate = Boolean(startDate);
+	const hasEndDate = Boolean(endDate);
+
+	if (!hasTargetDate) {
+		if (hasStartDate && !hasEndDate) {
+			throw new Error(
+				'Please provide an endDate argument to getMileageEntries()',
+			);
+		}
+		if (!hasStartDate && hasEndDate) {
+			throw new Error(
+				'Please provide a startDate argument to getMileageEntries()',
+			);
+		}
+	}
+
+	if (hasTargetDate && (hasStartDate || hasEndDate)) {
+		throw new Error(
+			'Please provide only a targetDate, or a startDate and endDate to getMileageEntries()',
+		);
+	}
+
+	let query = supabase.from('Miles').select('*');
+
+	if (targetedDate) {
+		query = query.eq('date', targetedDate);
+	}
+
+	if (startDate && endDate) {
+		query = query.gte('date', startDate).lte('date', endDate);
+	}
+
+	const { data, error } = await query;
 
 	if (error) {
 		console.log(error);
@@ -25,3 +63,25 @@ export const insertMileageEntry = async (entry) => {
 		throw new Error('Mileage entry could not be created');
 	}
 };
+
+/* Filters
+  .eq('column', 'Equal to')
+  .gt('column', 'Greater than')
+  .lt('column', 'Less than')
+  .gte('column', 'Greater than or equal to')
+  .lte('column', 'Less than or equal to')
+  .like('column', '%CaseSensitive%')
+  .ilike('column', '%CaseInsensitive%')
+  .is('column', null)
+  .in('column', ['Array', 'Values'])
+  .neq('column', 'Not equal to')
+
+  // Arrays
+  .contains('array_column', ['array', 'contains'])
+  .containedBy('array_column', ['contained', 'by'])
+
+  // Logical operators
+  .not('column', 'like', 'Negate filter')
+  .or('some_column.eq.Some value
+
+	*/
