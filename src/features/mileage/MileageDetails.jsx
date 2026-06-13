789255a735127bf5d4d07e492bde7168a9950c58
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-06-13 18:21:23 -0500

    feat: 🎸 add accordion behavior into MileageDetails entries

--- src/features/mileage/MileageDetails.jsx
+++ src/features/mileage/MileageDetails.jsx
@@ -8,11 +8,12 @@ import {
 import { LoaderCircle } from 'lucide-react';
 import ButtonLink from '../../ui/ButtonLink';
 import Button from '../../ui/Button';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export default function MileageDetails() {
 	const { timeFrame } = useParams();
 	const [openLocationIds, setOpenLocationIds] = useState([]);
+	const [expandedRowIds, setExpandedRowsIds] = useState([]);
 
 	let query = {
 		queryKey: ['miles'],
@@ -78,15 +79,13 @@ export default function MileageDetails() {
 
 			<table className="table-fixed">
 				<thead>
-					<tr className="border-b-2 border-slate-400">
-						<th className="border-b-2 border-slate-400 p-2">Date</th>
-						<th className="border-b-2 border-slate-400 p-2">
-							Starting odometer
-						</th>
-						<th className="border-b-2 border-slate-400 p-2">Ending odometer</th>
-						<th className="border-b-2 border-slate-400 p-2">Total miles</th>
-						<th className="border-b-2 border-slate-400 p-2">Location</th>
-						<th className="border-b-2 border-slate-400 p-2">Notes</th>
+					<tr>
+						<th className="p-2">Date</th>
+						<th className="p-2">Starting odometer</th>
+						<th className="p-2">Ending odometer</th>
+						<th className="p-2">Total miles</th>
+						<th className="p-2">Location</th>
+						<th className="p-2">Notes</th>
 					</tr>
 				</thead>
 
@@ -96,51 +95,76 @@ export default function MileageDetails() {
 						const hasMultipleLocations = entry.locations.length > 1;
 
 						return (
-							<tr className="border-b-2 border-b-slate-400 text-center font-medium text-slate-700">
-								<td className="px-2 py-3">{entry.date}</td>
-								<td className="px-6 py-3">{entry.initialMiles}</td>
-								<td className="px-6 py-3">{entry.endingMiles}</td>
-								<td className="px-6 py-3 font-semibold">{entry.totalMiles}</td>
-								<td className="px-5 py-3">
-									<ul className="flex flex-col text-sm font-semibold text-slate-600 hover:cursor-pointer">
-										<li>
-											{hasMultipleLocations ? (
-												<Button
-													className="w-48"
-													onClick={() => {
-														isOpen
-															? setOpenLocationIds(
-																	openLocationIds.filter(
-																		(id) => entry.id != id,
-																	),
-																)
-															: setOpenLocationIds([
-																	...openLocationIds,
-																	entry.id,
-																]);
-													}}
-												>
-													{
-														<p className="mb-0.5 font-bold">{`${entry.locations.length} locations`}</p>
-													}
-													<div
-														className={`mt-1.5 space-y-1 overflow-hidden font-semibold capitalize transition-all duration-400 ${isOpen ? 'max-h-40 opacity-100' : 'max-h-0 opacity-0'}`}
+							<React.Fragment>
+								<tr
+									className="cursor-pointer text-center font-medium text-slate-700"
+									onClick={() => {
+										expandedRowIds.includes(entry.id)
+											? setExpandedRowsIds(
+													expandedRowIds.filter((id) => id != entry.id),
+												)
+											: setExpandedRowsIds([...expandedRowIds, entry.id]);
+									}}
+								>
+									<td className="px-2 py-3">{entry.date}</td>
+									<td className="px-6 py-3">{entry.initialMiles}</td>
+									<td className="px-6 py-3">{entry.endingMiles}</td>
+									<td className="px-6 py-3 font-semibold">
+										{entry.totalMiles}
+									</td>
+									<td className="px-5 py-3">
+										<ul className="flex flex-col text-sm font-semibold text-slate-600 hover:cursor-pointer">
+											<li>
+												{hasMultipleLocations ? (
+													<Button
+														className="w-48"
+														onClick={() => {
+															isOpen
+																? setOpenLocationIds(
+																		openLocationIds.filter(
+																			(id) => entry.id != id,
+																		),
+																	)
+																: setOpenLocationIds([
+																		...openLocationIds,
+																		entry.id,
+																	]);
+														}}
 													>
-														{entry.locations.map((locationString) => (
-															<p className="wrap-break-word">
-																{locationString}
-															</p>
-														))}
-													</div>
-												</Button>
-											) : (
-												<p className="capitalize">{entry.locations}</p>
-											)}
-										</li>
-									</ul>
-								</td>
-								<td className="px-5 py-3">...</td>
-							</tr>
+														{
+															<p className="mb-0.5 font-bold">{`${entry.locations.length} locations`}</p>
+														}
+														<div
+															className={`mt-1.5 space-y-1 overflow-hidden font-semibold capitalize transition-all duration-400 ${isOpen ? 'max-h-40 opacity-100' : 'max-h-0 opacity-0'}`}
+														>
+															{entry.locations.map((locationString) => (
+																<p className="wrap-break-word">
+																	{locationString}
+																</p>
+															))}
+														</div>
+													</Button>
+												) : (
+													<p className="capitalize">{entry.locations}</p>
+												)}
+											</li>
+										</ul>
+									</td>
+									<td className="px-5 py-3">...</td>
+								</tr>
+
+								<tr className="border-b-2 border-slate-400">
+									<td>
+										<div
+											className={`overflow-hidden transition-all duration-300 ${expandedRowIds.includes(entry.id) ? 'max-h-60 opacity-100' : 'max-h-0 opacity-0'}`}
+										>
+											<Button>Edit</Button>
+											<Button>Save</Button>
+											{entry.notes && <Button>{entry.notes}</Button>}
+										</div>
+									</td>
+								</tr>
+							</React.Fragment>
 						);
 					})}
 				</tbody>
