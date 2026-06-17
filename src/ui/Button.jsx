598dc4dbb0175267a1660e2e06ec984ada7d8f77
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-06-17 17:29:43 -0500

    refactor: 💡 add twMerge to Button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,3 +1,5 @@
+import { twMerge } from 'tailwind-merge';
+
 export default function Button({
 	type = '',
 	disabled = false,
@@ -9,7 +11,10 @@ export default function Button({
 		<button
 			type={type}
 			disabled={disabled}
-			className={`${className} rounded-xl border-2 border-slate-500 bg-slate-50 p-3 text-center text-sm font-semibold tracking-wider text-slate-500 transition-all duration-150 hover:cursor-pointer hover:bg-slate-300 hover:text-slate-600 focus:outline-none focus-visible:border-emerald-500 focus-visible:shadow focus-visible:ring-3 focus-visible:ring-emerald-500 active:scale-95 active:border-slate-400/75 active:bg-slate-100 active:text-slate-600/75`}
+			className={twMerge(
+				`${className} rounded-xl border-2 border-slate-500 bg-slate-50 p-3 text-center text-sm font-semibold tracking-wider text-slate-500 transition-all duration-150 hover:cursor-pointer hover:bg-slate-300 hover:text-slate-600 focus:outline-none focus-visible:border-emerald-500 focus-visible:shadow focus-visible:ring-3 focus-visible:ring-emerald-500 active:scale-95 active:border-slate-400/75 active:bg-slate-100 active:text-slate-600/75`,
+				className,
+			)}
 			onClick={onClick}
 		>
 			{children}
