a1ea61dfe17e1de0f60bafe85165abed48bf78a8
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-06-27 13:40:25 -0500

    refactor: 💡 simplify stylings into variables

--- src/features/mileage/mileageDetailsGrid/MileageGridRow.jsx
+++ src/features/mileage/mileageDetailsGrid/MileageGridRow.jsx
@@ -2,51 +2,41 @@ import { twMerge } from 'tailwind-merge';
 
 export default function GridRow({
 	data,
-	headerCol = false,
+	isHeaderCol = false,
 	className,
-	onClick = null,
+	// eslint-disable-next-line no-unused-vars
+	isInEditMode = false,
 }) {
 	const { col1, col2, col3, col4, col5 } = data;
+
+	// styles
+	const headerRowStyling = isHeaderCol ? 'border-t-2' : '';
+	const cellBaseStyling = 'min-h-13 flex-1 rounded-xs border-r-2';
+	const firstColCellStyling = 'border-l-2';
+
 	return (
-		<div onClick={onClick} className="grid grid-cols-5">
+		// Container
+		<div className={twMerge('grid grid-cols-5 transition-all')}>
 			<div
 				className={twMerge(
-					`min-h-13 flex-1 rounded-xs border-r-2 border-l-2 ${headerCol && 'border-t-2'}`,
+					cellBaseStyling,
+					firstColCellStyling,
+					headerRowStyling,
 					className,
 				)}
 			>
 				{col1}
 			</div>
-			<div
-				className={twMerge(
-					`min-h-13 flex-1 rounded-xs border-r-2 ${headerCol && 'border-t-2'}`,
-					className,
-				)}
-			>
+			<div className={twMerge(cellBaseStyling, headerRowStyling, className)}>
 				{col2}
 			</div>
-			<div
-				className={twMerge(
-					`min-h-13 flex-1 rounded-xs border-r-2 ${headerCol && 'border-t-2'}`,
-					className,
-				)}
-			>
+			<div className={twMerge(cellBaseStyling, headerRowStyling, className)}>
 				{col3}
 			</div>
-			<div
-				className={twMerge(
-					`min-h-13 flex-1 rounded-xs border-r-2 ${headerCol && 'border-t-2'}`,
-					className,
-				)}
-			>
+			<div className={twMerge(cellBaseStyling, headerRowStyling, className)}>
 				{col4}
 			</div>
-			<div
-				className={twMerge(
-					`flex min-h-13 flex-1 items-center justify-center rounded-xs border-r-2 ${headerCol && 'border-t-2'}`,
-					className,
-				)}
-			>
+			<div className={twMerge(cellBaseStyling, headerRowStyling, className)}>
 				{col5}
 			</div>
 		</div>
