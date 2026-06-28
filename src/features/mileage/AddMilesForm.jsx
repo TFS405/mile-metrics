23dc3ee7c0d83fb46810f82e02ecd71e3e0a0e46
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-06-28 16:38:01 -0500

    feat: 🎸 integrate pessimistic update into form submit

--- src/features/mileage/AddMilesForm.jsx
+++ src/features/mileage/AddMilesForm.jsx
@@ -225,7 +225,7 @@ export default function AddMilesForm() {
 				disabled={isSubmitting}
 				className="rounded-xl border-2 border-slate-300/75 bg-white p-3 text-center text-sm font-semibold tracking-wider text-slate-500 transition-all duration-150 hover:cursor-pointer hover:bg-slate-50 focus:border-emerald-500 focus:ring-2 focus:ring-emerald-500 focus:outline-none active:scale-95 active:border-slate-400/75 active:bg-slate-100 active:text-slate-600/75"
 			>
-				Submit
+				{isSubmitting ? <span>...Submitting</span> : 'Submit'}
 			</Button>
 		</Form>
 	);
