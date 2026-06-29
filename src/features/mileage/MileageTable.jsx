43856fd7b4c11b45963144dd28ad60dc556c29c1
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-06-29 18:36:35 -0500

    feat: 🎸 make locations expandable, and stylize table

--- src/features/mileage/MileageTable.jsx
+++ src/features/mileage/MileageTable.jsx
@@ -1,8 +1,11 @@
 import {
 	flexRender,
 	getCoreRowModel,
+	getExpandedRowModel,
 	useReactTable,
 } from '@tanstack/react-table';
+import React from 'react';
+import Button from '../../ui/Button';
 
 export const MileageTable = ({ data = [] }) => {
 	const columns = [
@@ -27,59 +30,91 @@ export const MileageTable = ({ data = [] }) => {
 			header: 'Locations',
 		},
 	];
-	console.log(data);
+	// console.log(data);
 
 	const table = useReactTable({
 		data,
 		columns,
 		getCoreRowModel: getCoreRowModel(),
+		getExpandedRowModel: getExpandedRowModel(),
+		getRowCanExpand: (row) => row.original.locations?.length > 1,
 	});
 
 	return (
-		<table className="w-full">
-			<thead className="border-b-2 border-b-slate-400">
-				{table.getHeaderGroups().map((headerGroup) => (
-					<tr key={headerGroup.id}>
-						{headerGroup.headers.map((header) => (
-							<th key={header.id} className="pb-0.5">
-								{flexRender(
-									header.column.columnDef.header,
-									header.getContext(),
-								)}
-							</th>
-						))}
-					</tr>
-				))}
-			</thead>
+		// Container
+		<div className="shadow-sm">
+			{/* Header row */}
+			<div className="text-md grid grid-cols-5 border-t-2 border-b-2 border-slate-300 bg-gray-100 py-2 text-center font-semibold tracking-wide text-gray-600">
+				{table.getHeaderGroups().map((headerGroup) => {
+					return (
+						<React.Fragment key={headerGroup.id}>
+							{headerGroup.headers.map((header) => {
+								return (
+									<div key={header.id} className="items-center text-center">
+										{flexRender(
+											header.column.columnDef.header,
+											header.getContext(),
+										)}
+									</div>
+								);
+							})}
+						</React.Fragment>
+					);
+				})}
+			</div>
 
-			<tbody>
-				{table.getRowModel().rows.map((row) => (
-					<tr key={row.id} className="">
-						{row.getVisibleCells().map((cell) => {
-							const cellValue = cell.getValue();
-							const cellId = cell.column.id;
-							const cellDisplayValue = (() => {
-								if (cellId === 'locations') {
+			{/* Body rows */}
+			<div className="">
+				{table.getRowModel().rows.map((row, index) => {
+					const isInEvenColumn = Boolean(index % 2 === 0);
+					const evenColumnStyling = 'bg-gray-50 	0 h-full';
+					const oddColumnStyling = 'bg-slate-50 h-full';
+
+					return (
+						<div
+							className={`grid grid-cols-5 items-center border-b-2 border-slate-300 py-1.5 text-center ${isInEvenColumn ? evenColumnStyling : oddColumnStyling}`}
+							key={row.id}
+						>
+							{row.getVisibleCells().map((cell) => {
+								const cellType = cell.column.id;
+								const cellValue = cell.getValue();
+
+								if (cellType === 'locations') {
 									if (cellValue.length > 1) {
-										return `${cellValue.length} Locations`;
-									}
-									return cellValue[0];
-								}
-								return cellValue;
-							})(cellId);
+										return (
+											<div key={cell.id}>
+												<Button
+													onClick={row.getToggleExpandedHandler()}
+													className="mb-0.5 w-fit justify-self-center px-3.5 py-3"
+												>{`${cellValue.length} Locations`}</Button>
 
-							return (
-								<td
-									className="border-b-2 border-slate-300 py-2 text-center capitalize"
-									key={cell.id}
-								>
-									{cellDisplayValue}
-								</td>
-							);
-						})}
-					</tr>
-				))}
-			</tbody>
-		</table>
+												<div
+													className={`grid capitalize transition-all duration-200 ${row.getIsExpanded() ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}
+												>
+													<div className={`min-h-0 overflow-hidden`}>
+														{cellValue.map((location, index) => {
+															return (
+																<p key={`${cell.id}-${index}`}>{location}</p>
+															);
+														})}
+													</div>
+												</div>
+											</div>
+										);
+									}
+									return (
+										<p key={cell.id} className="capitalize">
+											{cellValue[0]}
+										</p>
+									);
+								}
+
+								return <p key={cell.id}>{cellValue}</p>;
+							})}
+						</div>
+					);
+				})}
+			</div>
+		</div>
 	);
 };
