66e935433d9fdd4a141958312c52ea080891cfb5
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-08-01 21:07:25 -0500

    feat: 🎸 allow custom sizing and classname props, add twMerge

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,10 +1,16 @@
-import { LoaderCircle } from 'lucide-react';
+import { Loader as LoaderSvg } from 'lucide-react';
+import { twMerge } from 'tailwind-merge';
 
-export default function Loader() {
+export default function Loader({ size, containerClassName = '' }) {
 	return (
-		<div className="flex min-h-screen items-center justify-center">
-			<LoaderCircle
-				size={45}
+		<div
+			className={twMerge(
+				'flex items-center justify-center',
+				containerClassName,
+			)}
+		>
+			<LoaderSvg
+				size={size}
 				className="animate-[spin_1.5s_ease-in-out_infinite]"
 			/>
 		</div>
