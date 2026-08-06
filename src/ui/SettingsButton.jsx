0002449a3b193ecaded3f3e7f04e480797fe484f
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-08-05 19:16:37 -0500

    style: 💄 make svg smaller and reduce padding on right of text

--- src/ui/SettingsButton.jsx
+++ src/ui/SettingsButton.jsx
@@ -1,9 +1,9 @@
 import { Cog } from 'lucide-react';
 
-export const SettingsButton = ({ onClick, sizeSVG = 23 }) => {
+export const SettingsButton = ({ onClick, sizeSVG = 21 }) => {
 	return (
 		<div
-			className="group active:slate-700 flex cursor-pointer items-center justify-start gap-1.5 text-slate-500 transition-all duration-125 hover:text-slate-700 active:scale-90"
+			className="group active:slate-700 flex cursor-pointer items-center justify-start gap-1 text-slate-500 transition-all duration-125 hover:text-slate-700 active:scale-90"
 			onClick={onClick}
 		>
 			<p className="font-data font-medium tracking-wide">Settings</p>
