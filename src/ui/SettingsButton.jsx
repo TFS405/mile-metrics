2f2aeef5e99e4827270ed6d75aae9978566a7d5b
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-08-08 15:09:05 -0500

    refactor: 💡 change  header text

--- src/ui/SettingsButton.jsx
+++ src/ui/SettingsButton.jsx
@@ -1,16 +1,16 @@
 import { Cog } from 'lucide-react';
 
 export const SettingsButton = ({ onClick, sizeSVG = 21 }) => {
-	return (
-		<div
-			className="group active:slate-700 flex cursor-pointer items-center justify-start gap-1 text-slate-500 transition-all duration-125 hover:text-slate-700 active:scale-90"
-			onClick={onClick}
-		>
-			<p className="font-data font-medium tracking-wide">Settings</p>
-			<Cog
-				size={sizeSVG}
-				className="transition-all duration-100 group-hover:rotate-450"
-			/>
-		</div>
-	);
+  return (
+    <div
+      className="group active:slate-700 flex cursor-pointer items-center justify-start gap-1 text-slate-500 transition-all duration-125 hover:text-slate-700 active:scale-90"
+      onClick={onClick}
+    >
+      <p className="font-data font-medium">Filter & Sort</p>
+      <Cog
+        size={sizeSVG}
+        className="transition-all duration-100 group-hover:rotate-450"
+      />
+    </div>
+  );
 };
