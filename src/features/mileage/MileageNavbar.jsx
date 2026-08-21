86de2a86d95f3876e3c6addfd86f546ed98d87dd
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-08-21 12:20:51 -0500

    refactor: 💡 rename component from Sidebar to Navbar

--- src/features/mileage/MileageNavbar.jsx
+++ src/features/mileage/MileageNavbar.jsx
@@ -1,39 +1,39 @@
 import { NavLink } from 'react-router';
 
-export default function Sidebar() {
-	return (
-		<div className="text- flex w-full items-center justify-between gap-2 border-b border-b-slate-600 px-3 py-2">
-			<NavLink
-				className={({ isActive }) =>
-					`${isActive ? 'text-emerald-700' : 'text-slate-600'} font-semibold tracking-tight`
-				}
-				to="/"
-			>
-				<h1>MileMetrics</h1>
-			</NavLink>
+export default function Navbar() {
+  return (
+    <div className="text- flex w-full items-center justify-between gap-2 border-b border-b-slate-600 px-3 py-2">
+      <NavLink
+        className={({ isActive }) =>
+          `${isActive ? 'text-emerald-700' : 'text-slate-600'} font-semibold tracking-tight`
+        }
+        to="/"
+      >
+        <h1>MileMetrics</h1>
+      </NavLink>
 
-			<ul className="mr-1 flex gap-5 text-xs font-semibold text-slate-600">
-				<li>
-					<NavLink
-						className={({ isActive }) =>
-							`${isActive && 'text-emerald-700'} 'text-slate-600'} font-semibold tracking-tight`
-						}
-						to={'/add-miles'}
-					>
-						Add Miles
-					</NavLink>
-				</li>
-				<li>
-					<NavLink
-						className={({ isActive }) =>
-							`${isActive && 'text-emerald-700'} 'text-slate-600'} font-semibold tracking-tight`
-						}
-						to={'view-miles'}
-					>
-						View miles
-					</NavLink>
-				</li>
-			</ul>
-		</div>
-	);
+      <ul className="mr-1 flex gap-5 text-xs font-semibold text-slate-600">
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              `${isActive && 'text-emerald-700'} 'text-slate-600'} font-semibold tracking-tight`
+            }
+            to={'/add-miles'}
+          >
+            Add Miles
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              `${isActive && 'text-emerald-700'} 'text-slate-600'} font-semibold tracking-tight`
+            }
+            to={'view-miles'}
+          >
+            View miles
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,14 @@
-import Sidebar from '../features/mileage/MileageNavbar';
+import Navbar from '../features/mileage/MileageNavbar';
 import { Outlet } from 'react-router';
 
 export default function AppLayout() {
-	return (
-		<div className="flex h-screen flex-col">
-			<Sidebar />
+  return (
+    <div className="flex h-screen flex-col">
+      <Navbar />
 
-			<main className="flex h-screen justify-center align-middle">
-				<Outlet />
-			</main>
-		</div>
-	);
+      <main className="flex h-screen justify-center align-middle">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
