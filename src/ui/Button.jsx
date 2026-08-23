823571a9b772e7d342777b7e26a06df33e972242
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-08-22 20:39:52 -0500

    refactor: 💡 replace native HTML button with Headless UI Button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,23 +1,24 @@
+import { Button as HeadlessButton } from '@headlessui/react';
 import { twMerge } from 'tailwind-merge';
 
 export default function Button({
-	type = 'button',
-	disabled = false,
-	className = '',
-	children = '',
-	onClick = undefined,
+  type = 'button',
+  disabled = false,
+  className = '',
+  children = '',
+  onClick = undefined,
 }) {
-	return (
-		<button
-			type={type}
-			disabled={disabled}
-			className={twMerge(
-				`rounded-xl border-2 border-slate-500 bg-slate-50 p-3 text-center text-sm font-semibold tracking-wider text-slate-500 transition-all duration-150 hover:cursor-pointer hover:bg-slate-300 hover:text-slate-600 focus:outline-none focus-visible:border-emerald-500 focus-visible:shadow focus-visible:ring-3 focus-visible:ring-emerald-500 active:scale-95 active:border-slate-400/75 active:bg-slate-100 active:text-slate-600/75`,
-				className,
-			)}
-			onClick={onClick}
-		>
-			{children}
-		</button>
-	);
+  return (
+    <HeadlessButton
+      type={type}
+      disabled={disabled}
+      className={twMerge(
+        `rounded-xl border-2 border-slate-500 bg-slate-50 p-3 text-center text-sm font-semibold tracking-wider text-slate-500 transition-all duration-150 hover:cursor-pointer hover:bg-slate-300 hover:text-slate-600 focus:outline-none focus-visible:border-emerald-500 focus-visible:shadow focus-visible:ring-3 focus-visible:ring-emerald-500 active:scale-95 active:border-slate-400/75 active:bg-slate-100 active:text-slate-600/75`,
+        className,
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </HeadlessButton>
+  );
 }
