32954b3c101caf1eeadb08dd48eec4651ad5bd25
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-08-30 09:08:43 -0500

    refactor: 💡 adjust padding on top row of form

--- src/features/mileage/AddMilesForm.jsx
+++ src/features/mileage/AddMilesForm.jsx
@@ -212,23 +212,26 @@ export default function AddMilesForm() {
   return (
     <Form
       onSubmit={handleSubmit(onSubmit, onError)}
-      className='flex max-w-4xl flex-col rounded-xl border-2 border-slate-300/75 p-3 shadow-sm'
+      className='font-data flex max-w-4xl flex-col rounded-xl border-2 border-slate-300 p-3 shadow-sm'
     >
-      <div className='font-data'>
-        {/* Date  */}
-        <div className='flex justify-start pt-1.5 pb-7 pl-2'>
+      <div className='grid grid-cols-[auto_1fr] pt-0.5 pb-5 pl-1'>
+        {/* DATE  */}
+        <div className='flex pl-0.5'>
           <DatePicker {...datePickerProps}>
             <DatePicker.Popover {...popoverProps} />
           </DatePicker>
         </div>
 
-        {/* Mileage */}
-        <div className='flex justify-evenly gap-7 pb-9'>
-          {/* Initial Miles */}
-          <div className='flex flex-1 flex-col'>
-            <FieldLabel className='self-center pb-1' htmlFor='initial-odometer'>
-              Starting Odometer
+        {/* INITIAL MILES */}
+        <div className='flex items-end justify-center gap-15'>
+          <div className='flex flex-col'>
+            <FieldLabel
+              className='self-center pb-1 text-lg font-medium'
+              htmlFor='initial-odometer'
+            >
+              Initial Miles
             </FieldLabel>
+
             <Controller
               name='initialMiles'
               control={control}
@@ -248,8 +251,8 @@ export default function AddMilesForm() {
             />
           </div>
 
-          {/* Ending Miles */}
-          <div className='flex flex-1 flex-col'>
+          {/* ENDING MILES */}
+          <div className='flex flex-col'>
             <FieldLabel className='self-center pb-1' htmlFor='odometer-end'>
               Ending Odometer
             </FieldLabel>
@@ -276,91 +279,90 @@ export default function AddMilesForm() {
             />
           </div>
         </div>
-
-        {/* Location selection */}
-        <div className='flex flex-col pb-6'>
-          {fields.map((field, index) => (
-            <div
-              key={field.id}
-              className='mb-3 flex flex-col justify-around pb-2'
-            >
-              {/* Location Selectors */}
-              <div className='flex justify-around pb-5'>
-                <MileageFormDateInput
-                  control={control}
-                  index={index}
-                  resetField={resetField}
-                />
-              </div>
-              {/* Checkboxes */}
-              <Fieldset
-                options={options(index)}
-                classNames={{
-                  container:
-                    'grid pr-3 w-full grid-cols-4 text-slate-600 font-data',
-                  field: 'gap-1 items-center',
-                }}
-              />
-
-              {fields.length > 1 && (
-                <ArrowDown
-                  size={20}
-                  className='absolute right-1/2 bottom-1.5 translate-x-1/2 text-slate-500'
-                />
-              )}
-            </div>
-          ))}
-
-          {/* Remove & Add location */}
-          <div className='flex justify-center gap-30'>
-            {/* Remove location */}
-            <Button
-              type='button'
-              className='hover:bg-slate-white border-slate-300 text-xs text-slate-400 shadow-xs hover:border-slate-400 hover:text-slate-500'
-              onClick={() => {
-                remove(-1);
-              }}
-            >
-              Remove location
-            </Button>
-            {/* Add another location */}
-            <Button
-              type='button'
-              className='hover:bg-slate-white border-slate-300 text-xs text-slate-400 shadow-xs hover:border-slate-400 hover:text-slate-500'
-              onClick={() => {
-                append({
-                  country: null,
-                  region: null,
-                  locality: null,
-                });
-              }}
-            >
-              Add location
-            </Button>
-          </div>
-        </div>
-
-        {/* Notes */}
-        <div className='flex flex-col pb-7'>
-          <FieldLabel className='mb-1 text-center' htmlFor='notes'>
-            Notes
-          </FieldLabel>
-          <textarea
-            {...register('notes')}
-            id='notes'
-            placeholder='Notes...'
-            className='h-full w-15/16 self-center rounded-xl border border-slate-300 bg-slate-50 p-3 shadow-sm focus:border-emerald-500 focus:ring-3 focus:ring-emerald-500 focus:outline-none'
-          ></textarea>
-        </div>
-
-        <Button
-          type='submit'
-          disabled={isSubmitting}
-          className='rounded-xl border-2 border-slate-300/75 bg-white p-3 text-center text-sm font-semibold tracking-wider text-slate-500 transition-all duration-150 hover:cursor-pointer hover:bg-slate-50 focus:border-emerald-500 focus:ring-2 focus:ring-emerald-500 focus:outline-none active:scale-95 active:border-slate-400/75 active:bg-slate-100 active:text-slate-600/75'
-        >
-          {isSubmitting ? <span>...Submitting</span> : 'Submit'}
-        </Button>
       </div>
+
+      <div className='flex flex-col pb-6'>
+        {fields.map((field, index) => (
+          <div
+            key={field.id}
+            className='mb-3 flex flex-col justify-around pb-2'
+          >
+            {/* Location Selectors */}
+            <div className='flex justify-around pb-5'>
+              <MileageFormDateInput
+                control={control}
+                index={index}
+                resetField={resetField}
+              />
+            </div>
+            {/* Checkboxes */}
+            <Fieldset
+              options={options(index)}
+              classNames={{
+                container:
+                  'grid pr-3 w-full grid-cols-4 text-slate-600 font-data',
+                field: 'gap-1 items-center',
+              }}
+            />
+
+            {fields.length > 1 && (
+              <ArrowDown
+                size={20}
+                className='absolute right-1/2 bottom-1.5 translate-x-1/2 text-slate-500'
+              />
+            )}
+          </div>
+        ))}
+
+        {/* Remove & Add location */}
+        <div className='flex justify-center gap-30'>
+          {/* Remove location */}
+          <Button
+            type='button'
+            className='hover:bg-slate-white border-slate-300 text-xs text-slate-400 shadow-xs hover:border-slate-400 hover:text-slate-500'
+            onClick={() => {
+              remove(-1);
+            }}
+          >
+            Remove location
+          </Button>
+          {/* Add another location */}
+          <Button
+            type='button'
+            className='hover:bg-slate-white border-slate-300 text-xs text-slate-400 shadow-xs hover:border-slate-400 hover:text-slate-500'
+            onClick={() => {
+              append({
+                country: null,
+                region: null,
+                locality: null,
+              });
+            }}
+          >
+            Add location
+          </Button>
+        </div>
+      </div>
+
+      {/* Notes */}
+      <div className='flex flex-col pb-7'>
+        <FieldLabel className='mb-1 text-center' htmlFor='notes'>
+          Notes
+        </FieldLabel>
+        <textarea
+          {...register('notes')}
+          id='notes'
+          placeholder='Notes...'
+          className='h-full w-15/16 self-center rounded-xl border border-slate-300 bg-slate-50 p-3 shadow-sm focus:border-emerald-500 focus:ring-3 focus:ring-emerald-500 focus:outline-none'
+        ></textarea>
+      </div>
+
+      <Button
+        type='submit'
+        disabled={isSubmitting}
+        className='rounded-xl border-2 border-slate-300/75 bg-white p-3 text-center text-sm font-semibold tracking-wider text-slate-500 transition-all duration-150 hover:cursor-pointer hover:bg-slate-50 focus:border-emerald-500 focus:ring-2 focus:ring-emerald-500 focus:outline-none active:scale-95 active:border-slate-400/75 active:bg-slate-100 active:text-slate-600/75'
+      >
+        {isSubmitting ? <span>...Submitting</span> : 'Submit'}
+      </Button>
     </Form>
   );
 }
