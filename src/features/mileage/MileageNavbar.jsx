2132c2304ba1e1266c72b48d9df3b4a8df72c672
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-05 18:41:32 -0500

    feat: 🎸 add animated active nav underline

--- src/features/mileage/MileageNavbar.jsx
+++ src/features/mileage/MileageNavbar.jsx
@@ -1,40 +1,38 @@
 import { NavLink } from 'react-router';
 import logo from '../../assets/mileMetricsLogo.png';
+import cn from '../../utils/cn';
 
 export default function Navbar() {
-  return (
-    <div className='mb-6 flex items-center justify-between gap-2 border-b border-b-gray-300 bg-gray-200/75 px-3 py-2 shadow-xs'>
-      <NavLink
-        className={({ isActive }) =>
-          `${isActive ? 'text-emerald-700' : 'text-slate-600'} font-semibold tracking-tight`
-        }
-        to='/'
-      >
-        <img src={logo} alt='MileMetrics' className='h-8 w-auto' />
-      </NavLink>
+  const navLinkStyling = (isActive) =>
+    cn(
+      `relative flex h-full items-center font-semibold tracking-tight text-gray-600 after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:origin-center after:scale-x-0 after:bg-emerald-500 after:transition-transform after:duration-150`,
 
-      <ul className='mr-1 flex gap-5 text-xs font-semibold text-slate-600'>
-        <li>
-          <NavLink
-            className={({ isActive }) =>
-              `${isActive && 'text-emerald-700'} 'text-slate-600'} font-semibold tracking-tight`
-            }
-            to={'/add-miles'}
-          >
-            Add Miles
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) =>
-              `${isActive && 'text-emerald-700'} 'text-slate-600'} font-semibold tracking-tight`
-            }
-            to={'view-miles'}
-          >
-            View miles
-          </NavLink>
-        </li>
-      </ul>
+      isActive && 'after:scale-x-100',
+    );
+
+  return (
+    <div className='mb-6 flex h-13 items-center justify-between gap-2 border-b border-b-gray-300 bg-gray-200/75 px-3 text-xs shadow-xs'>
+      <div className='flex h-full place-items-center'>
+        <NavLink className={({ isActive }) => navLinkStyling(isActive)} to='/'>
+          <img src={logo} alt='MileMetrics' className='h-8 w-auto' />
+        </NavLink>
+      </div>
+
+      <div className='flex h-full gap-3 pr-2'>
+        <NavLink
+          className={({ isActive }) => navLinkStyling(isActive)}
+          to={'/add-miles'}
+        >
+          Add Miles
+        </NavLink>
+
+        <NavLink
+          className={({ isActive }) => navLinkStyling(isActive)}
+          to={'view-miles'}
+        >
+          View miles
+        </NavLink>
+      </div>
     </div>
   );
 }
