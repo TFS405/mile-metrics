aad4eeaaf46ad98463911674a299ab52e0c31420
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-05 16:46:08 -0500

    refactor: 💡 migrate to Base UI and remove default styles

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,5 @@
-import { Button as HeadlessButton } from '@headlessui/react';
 import { twMerge } from 'tailwind-merge';
+import { Button as BaseUIButton } from '@base-ui/react/button';
 
 export default function Button({
   type = 'button',
@@ -9,16 +9,13 @@ export default function Button({
   onClick = undefined,
 }) {
   return (
-    <HeadlessButton
+    <BaseUIButton
       type={type}
       disabled={disabled}
-      className={twMerge(
-        `rounded-xl border-2 border-slate-500 bg-slate-50 p-3 text-center text-sm font-semibold tracking-wider text-slate-500 transition-all duration-150 hover:cursor-pointer hover:bg-slate-300 hover:text-slate-600 focus:outline-none focus-visible:border-emerald-500 focus-visible:shadow focus-visible:ring-3 focus-visible:ring-emerald-500 active:scale-95 active:border-slate-400/75 active:bg-slate-100 active:text-slate-600/75`,
-        className,
-      )}
+      className={twMerge(className)}
       onClick={onClick}
     >
       {children}
-    </HeadlessButton>
+    </BaseUIButton>
   );
 }
