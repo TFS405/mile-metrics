a06d1216158be6e195825ee08e43ec793fb65ac6
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-05 16:43:51 -0500

    feat: 🎸 add MileMetrics logo and refine styling

--- src/features/mileage/MileageNavbar.jsx
+++ src/features/mileage/MileageNavbar.jsx
@@ -1,18 +1,19 @@
 import { NavLink } from 'react-router';
+import logo from '../../assets/mile-metrics-logo-v7.svg';
 
 export default function Navbar() {
   return (
-    <div className="mb-6 flex w-full items-center justify-between gap-2 border-b border-b-slate-600 px-3 py-2">
+    <div className='mb-6 flex items-center justify-between gap-2 border-b border-b-gray-300 bg-gray-200/75 px-3 py-2 shadow-xs'>
       <NavLink
         className={({ isActive }) =>
           `${isActive ? 'text-emerald-700' : 'text-slate-600'} font-semibold tracking-tight`
         }
-        to="/"
+        to='/'
       >
-        <h1>MileMetrics</h1>
+        <img src={logo} className='h-8 w-auto' />
       </NavLink>
 
-      <ul className="mr-1 flex gap-5 text-xs font-semibold text-slate-600">
+      <ul className='mr-1 flex gap-5 text-xs font-semibold text-slate-600'>
         <li>
           <NavLink
             className={({ isActive }) =>
