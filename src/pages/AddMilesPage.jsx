a26b84d27cfdc0bb0bfbe1218a4d6ced7bbbe217
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-05 16:49:02 -0500

    refactor: 💡 modularize form controls and simplify page layout

--- src/pages/AddMilesPage.jsx
+++ src/pages/AddMilesPage.jsx
@@ -1,9 +1,5 @@
 import AddMilesForm from '../features/mileage/AddMilesForm';
 
 export default function AddMilesPage() {
-	return (
-		<main className="flex h-full w-9/10 flex-col md:w-5/10">
-			<AddMilesForm />
-		</main>
-	);
+  return <AddMilesForm />;
 }
