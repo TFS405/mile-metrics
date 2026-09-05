ba1c0c053954923a15c98e3d7e3b33337259bf2c
Cameron Lozano <cameron.lozano1337@gmail.com>
2026-09-05 16:47:05 -0500

    refactor: 💡 merge custom classes and refine default styling

--- src/ui/FieldLabel.jsx
+++ src/ui/FieldLabel.jsx
@@ -1,14 +1,19 @@
+import { twMerge } from 'tailwind-merge';
+
 export default function sectionTitle({
-	children,
-	htmlFor = '',
-	className = '',
+  children,
+  htmlFor = '',
+  className = '',
 }) {
-	return (
-		<label
-			htmlFor={htmlFor}
-			className={`text-sm tracking-tight text-slate-600 capitalize ${className}`}
-		>
-			{children}
-		</label>
-	);
+  return (
+    <label
+      htmlFor={htmlFor}
+      className={twMerge(
+        `text-sm font-medium tracking-tight text-gray-600/90 capitalize`,
+        className,
+      )}
+    >
+      {children}
+    </label>
+  );
 }
